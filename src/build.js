20919171d5b55cd2b488e9047681ecf719dc4946
Henrik L <helagro@outlook.com>
2023-11-17 14:41:51 +0100

    Change structure

---
+++ src/build.js
@@ -0,0 +1,34 @@
+const Handlebars = require('handlebars')
+const fs = require('fs')
+
+const languages = [
+  {
+    inputName: 'english.json',
+    outputName: 'index.html',
+  },
+  {
+    inputName: 'swedish.json',
+    outputName: 'se.html',
+  },
+]
+
+for (const language of languages) {
+  compileLanguage(language)
+}
+
+function compileLanguage(lang) {
+  const templateSource = fs.readFileSync(
+    'source/template.html',
+    'utf8'
+  )
+  const contentStr = fs.readFileSync(
+    `source/${lang.inputName}`,
+    'utf8'
+  )
+
+  const contentObj = JSON.parse(contentStr)
+  const template = Handlebars.compile(templateSource)
+  const result = template(contentObj)
+
+  fs.writeFileSync(`docs/${lang.outputName}`, result)
+}
