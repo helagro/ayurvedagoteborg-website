cea49cb824c68a43e91cd852394c2afe7db67789
Henrik L <helagro@outlook.com>
2023-11-17 15:36:51 +0100

    Update templates

--- src/build.js
+++ src/build.js
@@ -1,36 +1,64 @@
 const fs = require('fs')
+const fse = require('fs-extra')
 const Handlebars = require('handlebars')
 
 const PARTIALS_DIR = 'components'
 const TEMPLATES_DIR = 'templates'
-const DESTINATION_DIR = 'dist'
+const DESTINATION_DIR = '../dist'
+const ASSETS_DIR = 'assets'
 
-fs.readdirSync(PARTIALS_DIR).forEach((filename) => {
-  const partialSource = fs.readFileSync(
-    `${PARTIALS_DIR}/${filename}`,
-    'utf8'
-  )
-  Handlebars.registerPartial(
-    filename.split('.')[0],
-    partialSource
-  )
-})
+function registerPartials() {
+    fs.readdirSync(PARTIALS_DIR).forEach((filename) => {
+        const partialSource = fs.readFileSync(
+            `${PARTIALS_DIR}/${filename}`,
+            'utf8'
+        )
+        Handlebars.registerPartial(
+            filename.split('.')[0],
+            partialSource
+        )
+    })
+}
 
-fs.readdirSync(TEMPLATES_DIR).forEach((filename) => {
-  const templateSource = fs.readFileSync(
-    `${TEMPLATES_DIR}/${filename}`,
-    'utf8'
-  )
-  const compiledTemplate =
-    Handlebars.compile(templateSource)
+function compileTemplates() {
+    fs.readdirSync(TEMPLATES_DIR).forEach((filename) => {
+        const templateSource = fs.readFileSync(
+            `${TEMPLATES_DIR}/${filename}`,
+            'utf8'
+        )
 
-  const output = compiledTemplate({})
-  console.log(output)
+        const compiledTemplate =
+            Handlebars.compile(templateSource)
+        const output = compiledTemplate({})
 
-  const filenameNoExt = filename.split('.')[0]
+        const filenameNoExt = filename.split('.')[0]
+        fs.writeFileSync(
+            `${DESTINATION_DIR}/${filenameNoExt}.html`,
+            output
+        )
+    })
+}
 
-  fs.writeFileSync(
-    `${DESTINATION_DIR}/${filenameNoExt}.html`,
-    output
-  )
-})
+function copyAssets() {
+    fs.readdirSync(ASSETS_DIR).forEach((folderName) => {
+        const newFolderName = `../dist/${folderName}`
+
+        fse.copySync(ASSETS_DIR, newFolderName, {
+            overwrite: true,
+            errorOnExist: false,
+        })
+    })
+}
+
+;(() => {
+    try {
+        fs.rmdirSync(DESTINATION_DIR, { recursive: true })
+    } catch (e) {
+        console.log(e)
+    }
+    fs.mkdirSync(DESTINATION_DIR)
+
+    registerPartials()
+    compileTemplates()
+    copyAssets()
+})()
