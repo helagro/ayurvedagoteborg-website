b2f7b0338cd4090f7873e61427658c84d3717183
Henrik L <helagro@outlook.com>
2024-12-30 18:21:43 +0100

    Update file names

--- src/build.js
+++ src/build.js
@@ -4,53 +4,50 @@ const Handlebars = require('handlebars')
 const { exit } = require('process')
 const minify = require('html-minifier').minify
 
+/* ------------------------ CONSTANTS ----------------------- */
+
 const PARTIALS_DIR = 'components'
 const TEMPLATES_DIR = 'templates'
 const DESTINATION_DIR = '../dist'
 const ASSETS_DIR = 'assets'
 
+const CONTEXT = {
+    base_address: 'https://ayurvedagoteborg.se',
+}
+
+/* -------------------- SETUP HANDLEBARS -------------------- */
+
 function registerPartials() {
     fs.readdirSync(PARTIALS_DIR).forEach((filename) => {
-        const partialSource = fs.readFileSync(
-            `${PARTIALS_DIR}/${filename}`,
-            'utf8'
-        )
-        Handlebars.registerPartial(
-            filename.split('.')[0],
-            partialSource
-        )
+        const partialSource = fs.readFileSync(`${PARTIALS_DIR}/${filename}`, 'utf8')
+        Handlebars.registerPartial(filename.split('.')[0], partialSource)
     })
 }
+
+/* ------------------------- COMPILE ------------------------ */
 
 function compileTemplates(dir = TEMPLATES_DIR) {
     fs.readdirSync(dir).forEach((filename) => {
         const path = `${dir}/${filename}`
 
-        if (fs.statSync(path).isDirectory())
-            return compileTemplates(path)
+        // Recurse if directory
+        if (fs.statSync(path).isDirectory()) return compileTemplates(path)
 
+        // Ignore non-handlebars files
         if (!filename.endsWith('.hbs')) return
 
-        const template = fs.readFileSync(path, 'utf8')
+        const templateFile = fs.readFileSync(path, 'utf8')
 
         try {
-            const compiledTemplate =
-                Handlebars.compile(template)
-            const compiled = compiledTemplate({})
+            const template = Handlebars.compile(templateFile)
+            const compiled = template(CONTEXT)
             const minimised = minify(compiled, {
                 collapseWhitespace: true,
                 removeComments: true,
             })
 
-            const newFolderPath = dir.replace(
-                TEMPLATES_DIR,
-                DESTINATION_DIR
-            )
-            writeOutputFile(
-                newFolderPath,
-                filename,
-                minimised
-            )
+            const newFolderPath = dir.replace(TEMPLATES_DIR, DESTINATION_DIR)
+            writeOutputFile(newFolderPath, filename, minimised)
         } catch (e) {
             console.error(`Error compiling ${filename}`)
             console.error(e)
@@ -68,26 +65,23 @@ function writeOutputFile(folderPath, filename, content) {
             recursive: true,
         })
 
-    fs.writeFileSync(
-        `${folderPath}/${filenameNoExt}.html`,
-        content
-    )
+    fs.writeFileSync(`${folderPath}/${filenameNoExt}.html`, content)
 }
+
+/* ------------------------- ASSETS ------------------------- */
 
 function copyAssets() {
     fs.readdirSync(ASSETS_DIR).forEach((folderName) => {
         const newFolderName = `${DESTINATION_DIR}/${folderName}`
 
-        fse.copySync(
-            `${ASSETS_DIR}/${folderName}`,
-            newFolderName,
-            {
-                overwrite: true,
-                errorOnExist: false,
-            }
-        )
+        fse.copySync(`${ASSETS_DIR}/${folderName}`, newFolderName, {
+            overwrite: true,
+            errorOnExist: false,
+        })
     })
 }
+
+/* -------------------------- START ------------------------- */
 
 ;(() => {
     try {
